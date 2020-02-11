0b31f969736cfe02c874cdac327af3de6e0b7382
valeria-laynes <60258591+valeria-laynes@users.noreply.github.com>
2020-02-10 23:54:47 -0500

    Add files via upload

---
+++ HW04.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score
+
+
+# In[2]:
+
+
+#Importing and partitioning dataset
+dataset = pd.read_excel('C:\\Users\\valer\\OneDrive\\Documentos\\Escritorio\\hw04.xlsx')
+dataset['Is'] = dataset['Is'].map({'Home':1, 'Away':0}).astype(int)
+dataset['Opponent'] = dataset['Opponent'].map({'In':1, 'Out':0}).astype(int)
+dataset['Media'] = dataset['Media'].map({'NBC':0, 'ABC':1, 'ESPN':2, 'FOX':3, 'CBS':4}).astype(int)
+dataset['Label'] = dataset['Label'].map({'Win':1, 'Lose':0, 0:0}).astype(int)
+xtrain = dataset.loc[0:24,'Is':'Media']
+ytrain = dataset.loc[0:24,'Label']
+xtest = dataset.loc[24:36,'Is':'Media']
+ytest = dataset.loc[24:36,'Label']
+
+
+# In[3]:
+
+
+#NBM
+gnb = GaussianNB()
+ypred = gnb.fit(xtrain, ytrain).predict(xtest)
+print(ypred)
+print(ytest)
+
+
+# In[4]:
+
+
+#Performance Metrics
+print('Accuracy: ')
+print(accuracy_score(ytest, ypred))
+print('Precision: ')
+print(precision_score(ytest, ypred))
+print('Recall: ')
+print(recall_score(ytest, ypred))
+print('F1: ')
+print(f1_score(ytest, ypred))
+
+
+# In[ ]:
+
+
+
+
